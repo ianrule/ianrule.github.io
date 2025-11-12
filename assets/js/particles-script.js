7e9116c731948c528d42fee0f961a0a2d09e74d0
Ian Rule <rule.ian@gmail.com>
2025-11-11 22:35:06 -0700

    Updates

--- assets/js/particles-script.js
+++ assets/js/particles-script.js
@@ -13,18 +13,10 @@ particlesJS('particles-js',
             "value": ["#ffffff", ]
         },
         "shape": {
-            "type": "circle",
+            "type": "star",
             "stroke": {
                 "width": 0,
                 "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 4
-            },
-            "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
             }
         },
         "opacity": {
